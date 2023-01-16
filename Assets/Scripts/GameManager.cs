48c41d9d697da247f840d7ee3d323da14d2c18e3
HarmoNick777 <harmonick777@gmail.com>
2023-01-16 16:16:37 +0100

    Lesson 5.3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] List<GameObject> targets;
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI gameOverText;
+    [SerializeField] Button restartButton;
+    public bool isGameActive;
+
     private float spawnRate = 1;
     private int score;
 
     void Start()
     {
-        StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(score);
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
     }
 
     void Update()
@@ -24,7 +31,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -36,5 +43,17 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
